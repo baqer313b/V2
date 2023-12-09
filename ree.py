ecc13bf1626512a18f5d7a2902787c579a344a9a
baqer313b <152201614+baqer313b@users.noreply.github.com>
2023-12-09 21:18:34 +0300

    Update ree.py

--- ree.py
+++ ree.py
@@ -1,1 +1,79 @@
-p
+import requests,bs4,json,os,sys,random,datetime,time,re
+import urllib3,rich,base64
+from rich.table import Table as me
+from rich.console import Console as sol
+from bs4 import BeautifulSoup as sop
+from concurrent.futures import ThreadPoolExecutor as tred
+from rich.console import Group as gp
+from rich.panel import Panel as nel
+from rich.markdown import Markdown as mark
+from rich.columns import Columns as col
+from rich import pretty
+from rich.text import Text as tekz
+from time import localtime as lt
+import requests,time,sys,os
+import random
+import json
+import uuid
+#import pyfiglet,users
+import os
+import threading
+from user_agent import generate_user_agent
+from faker import Faker
+import webbrowser
+webbrowser.open('https://t.me/py_1hon1')
+import webbrowser
+import requests,time,pyfiglet,datetime
+now = datetime.datetime.today()
+
+
+
+import time
+import sys
+import os
+import requests,time,pyfiglet,datetime
+now = datetime.datetime.today()
+
+now = datetime.datetime.today()
+mm = str(now.month)
+dd = str(now.day)
+yyyy = str(now.year)
+hour = str(now.hour)
+mi = str(now.minute)
+ss = str(now.second)
+t=(mm + "/" + dd + "/" + yyyy + " " + hour + ":" + mi + ":" + ss)
+
+
+hours = (now.hour)
+x = datetime.datetime.now()
+g= datetime.datetime(2023, 1, 12, 12, 10 ,9)
+
+
+if (x.strftime("%x"))>(g.strftime("%x")):
+ print('\033[1;32m تـم ايقاف الاداة من قبل نمرود العراقي') 
+ time.sleep(1)
+ print('\033[1;41m راسلني لتفعيلها') 
+ time.sleep(1)
+ print('\033[1;12m آحبڪم ')
+ time.sleep(1)
+ print('\033[1;61m 1')
+ time.sleep(1)
+ print('\033[1;72m 2')
+ time.sleep(1)
+ print('\033[1;31m 3')
+ time.sleep(1)
+ print('\033[1;32m 4')
+ time.sleep(1)
+ print('\033[1;31m 5')
+ time.sleep(1)
+ print('\033[1;32m 6')
+ time.sleep(1)
+ print('\033[1;31m 7')
+ time.sleep(1)
+ print('\033[1;32m 8')
+ time.sleep(1)
+ print('\033[1;31m 9')
+ time.sleep(1)
+ print('\033[1;32m10')
+ webbrowser.open('https://t.me/M_T_F')
+ exit()
